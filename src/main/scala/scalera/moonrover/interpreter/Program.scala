60bc253857ea4885684404b169e8102a4c27b978
JSantosP <javiersantospaniego@gmail.com>
2015-11-20 00:33:44 +0100

    Added coveralls

--- src/main/scala/scalera/moonrover/interpreter/Program.scala
+++ src/main/scala/scalera/moonrover/interpreter/Program.scala
@@ -1,22 +1,28 @@
 package scalera.moonrover.interpreter
 
-import scalera.moonrover.interpreter.Program.LineId
+import scalera.moonrover.interpreter.Program.{Index, LineId}
 
-case class Program[S](commands: Map[LineId,Command[S]]) {
+case class Program[S](commands: Map[LineId, Command[S]]) {
 
   require(
-    commands.size > 0,
+    commands.nonEmpty,
     "Empty program! Define some commands.")
 
   require(
     !commands.keys.exists(_ < 0),
     "Command lines with negative identifier? Seriously?")
 
+  val lines: Seq[(LineId, Index)] = commands.toSeq.sortWith {
+    case ((thisLine, _), (thatLine, _)) => thisLine < thatLine
+  }.map(_._1).zipWithIndex.map(_.swap)
+
 }
 
 object Program {
 
   type LineId = Int
+
+  type Index = Int
 
 }
 
