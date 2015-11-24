982c07bc44d097e1c00c8fc6506cc5af3381d62e
Javier Santos <javiersantospaniego@gmail.com>
2015-11-24 19:09:48 +0100

    Changed way to evaluate command sequence (Not finished yet)

--- src/main/scala/scalera/moonrover/interpreter/Interpreter.scala
+++ src/main/scala/scalera/moonrover/interpreter/Interpreter.scala
@@ -2,38 +2,43 @@ package scalera.moonrover.interpreter
 
 import scala.annotation.tailrec
 
+import scalera.moonrover.interpreter.Program.LineId
+
 /**
-  * An [[Interpreter]] works as a Turing Machine
-  * that evaluates some command list, changing
-  * its 'memory' state.
-  * @param state Internal interpreter's state.
-  * @param commandsLeft Pending commands to be executed.
-  * @tparam S State type.
-  */
+ * An [[Interpreter]] works as a Turing Machine
+ * that evaluates some command list, changing
+ * its 'memory' state.
+ * @param state Internal interpreter's state.
+ * @tparam S State type.
+ */
 case class Interpreter[S](
                            state: State[S],
-                           commandsLeft: Stream[Command[S]]) {
+                           program: Program[S],
+                           currentProgramLine: Option[LineId]) {
 
   /**
-    * Evaluates next command,
-    * returning a new immutable [[Interpreter]].
-    * @return
-    */
-  def eval: Interpreter[S] = commandsLeft match {
-    case nextCommand #:: rest =>
-      Interpreter(nextCommand.perform(state), rest)
-    case _ => this
-  }
+   * Evaluates next command,
+   * returning a new immutable [[Interpreter]].
+   * @return
+   */
+  def eval: Interpreter[S] = currentProgramLine.map { line =>
+    val newState = program.commands(line).perform(state)
+    Interpreter(newState, program, program.nextLine(line)(newState))
+  }.getOrElse(this)
 
   /**
-    * Evaluates all pending commands,
-    * @return A new immutable [[Interpreter]].
-    */
+   * Evaluates all pending commands,
+   * @return A new immutable [[Interpreter]].
+   */
   @tailrec
-  final def fullEval: Interpreter[S] = commandsLeft match {
-    case commands if commands == Stream.empty => this
+  final def fullEval: Interpreter[S] = currentProgramLine match {
+    case None => this
     case _ => this.eval.fullEval
   }
+}
 
+object Interpreter {
 
+  def apply[S](state: State[S], program: Program[S]): Interpreter[S] =
+    new Interpreter(state, program, program.nextLine()(state))
 }
