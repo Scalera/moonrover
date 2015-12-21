f6b8b358bfb682e804471a4f18135a9a0849d421
JSantosP <javiersantospaniego@gmail.com>
2015-12-21 21:47:16 +0100

    Added background images

--- display/src/main/scala/scalera/moonrover/display/SimulatorRender.scala
+++ display/src/main/scala/scalera/moonrover/display/SimulatorRender.scala
@@ -1,6 +1,9 @@
 package scalera.moonrover.display
 
+import org.scalajs.dom
 import org.scalajs.dom.CanvasRenderingContext2D
+import scalatags.JsDom.all._
+import org.scalajs.dom.raw.{HTMLImageElement, HTMLElement, ImageData}
 import scalera.moonrover.Simulator
 
 object SimulatorRender {
@@ -19,11 +22,24 @@ object SimulatorRender {
   def renderSimulator(
                        simulator: Simulator)(
     implicit ctx: CanvasRenderingContext2D): Unit = {
-    //TODO renderSimulator
+
+    //Background
+
+    val imageSelector: Int = (
+      simulator.state.value.foundParachute(simulator.roverId1),
+      simulator.state.value.foundParachute(simulator.roverId2)) match {
+      case (true,true) => 1
+      case (false,false) => 2
+      case (false,true) => 3
+      case (true,false) => 4
+    }
+    val image = dom.document.createElement("img").asInstanceOf[HTMLImageElement]
+    image.src = s"canvas-0$imageSelector.png"
+    ctx.drawImage(image,0,0)/*
     ctx.fillText(
-      simulator.toString,
-      100,
-      100)
+      "scalera.es",//simulator.toString,
+      718,
+      575)*/
   }
 
   /**
@@ -32,6 +48,7 @@ object SimulatorRender {
    * @return
    */
   def clear()(implicit ctx: CanvasRenderingContext2D): Unit = {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    //ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
   }
+
 }
